d6c1a10968e31bb3dbf52a56e9728101bbc7e56c
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-09-29 16:53:46 -0600

    Fix logo ico

--- client/src/views/DocenteView.jsx
+++ client/src/views/DocenteView.jsx
@@ -29,42 +29,6 @@ const DocenteView = () => {
       nombreCompleto: "Laura Smith",
       telefono: "77745678",
     },
-    {
-      fotoSrc: "https://i.pravatar.cc/150?u=e042581f4e29026024d",
-      uuid: "UUID5",
-      nombreCompleto: "Pedro Rodríguez",
-      telefono: "32109876",
-    },
-    {
-      fotoSrc: "https://i.pravatar.cc/150?u=f042581f4e29026024d",
-      uuid: "UUID6",
-      nombreCompleto: "María González",
-      telefono: "99933322",
-    },
-    {
-      fotoSrc: "https://i.pravatar.cc/150?u=g042581f4e29026024d",
-      uuid: "UUID7",
-      nombreCompleto: "Carlos Fernández",
-      telefono: "44488899",
-    },
-    {
-      fotoSrc: "https://i.pravatar.cc/150?u=h042581f4e29026024d",
-      uuid: "UUID8",
-      nombreCompleto: "Isabel López",
-      telefono: "55566677",
-    },
-    {
-      fotoSrc: "https://i.pravatar.cc/150?u=i042581f4e29026024d",
-      uuid: "UUID9",
-      nombreCompleto: "Luis Martínez",
-      telefono: "12345678",
-    },
-    {
-      fotoSrc: "https://i.pravatar.cc/150?u=j042581f4e29026024d",
-      uuid: "UUID10",
-      nombreCompleto: "Elena Ramírez",
-      telefono: "98765432",
-    },
   ];
   return (
     <div className="flex w-11/12 flex-col mx-auto">
