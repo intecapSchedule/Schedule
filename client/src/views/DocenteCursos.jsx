bdc1db00701663cf48e49811f75c338c8a317b4a
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-10-18 16:24:14 -0600

    Add card about Cursos and fix colors

--- client/src/views/DocenteCursos.jsx
+++ client/src/views/DocenteCursos.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
+import { contexto } from "../context/ContextProvider";
 import API_URL from "../config";
 
-const DocenteCursos = () => {
-  const [cursos, setCursos] = useState([]);
+const DocentecursosDocente = () => {
+  const [cursosDocente, setcursosDocenteDocente] = useState([]);
+  //Contexto para recargar la lista de cursos
+  const { cursos } = useContext(contexto);
 
-  const obtenerCursos = async () => {
+  const obtenercursosDocente = async () => {
     const usuario = JSON.parse(localStorage.getItem("demasdatosINTECAP"));
     try {
       const response = await fetch(`${API_URL}/user/getbyid/${usuario._id}`, {
@@ -15,20 +18,20 @@ const DocenteCursos = () => {
         credentials: "include",
       });
       if (!response.ok) {
-        console.log("Error al obtener los cursos");
-        throw new Error("Error al filtrar los cursos", {});
+        console.log("Error al obtener los cursosDocente");
+        throw new Error("Error al filtrar los cursosDocente", {});
       }
 
       const data = await response.json();
-      setCursos(data);
+      setcursosDocenteDocente(data);
     } catch (error) {}
   };
 
   useEffect(() => {
-    obtenerCursos();
-  }, []);
+    obtenercursosDocente();
+  }, [cursos]);
 
-  const courses = cursos;
+  const courses = cursosDocente;
 
   courses.sort((a, b) => new Date(a.fechaInicio) - new Date(b.fechaInicio));
 
@@ -70,10 +73,23 @@ const DocenteCursos = () => {
     return colores[Math.floor(Math.random() * colores.length)];
   };
 
+  const diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+  const diasColores = {
+    Lunes: "bg-[#94c8ff] text-black",
+    Martes: "bg-[#ff94c8] text-black",
+    Miércoles: "bg-[#c8ff94] text-black",
+    Jueves: "bg-[#8585ff] text-black",
+    Viernes: "bg-[#ff8585] text-black",
+    Sábado: "bg-[#85ff85] text-black",
+    Domingo: "bg-fuchsia-200 text-black",
+  };
+
   return (
     <>
       <div className="mx-auto w-11/12 relative border-1 shadow-md p-4 rounded-2xl mb-8">
-        <h1 className="text-3xl font-bold text-center mb-3 -mt-1 text-primary">Proyección de cursos en el año</h1>
+        <h1 className="text-3xl font-bold text-center mb-3 -mt-1 text-primary">
+          Proyección de cursosDocente en el años
+        </h1>
         <div className="flex justify-between">
           {months.map((month) => (
             <div key={month.name} className="text-center w-1/12 border-1 border-blue-600 overflow-hidden px-1">
@@ -140,11 +156,61 @@ const DocenteCursos = () => {
           })}
         </div>
       </div>
-      <div className="mx-auto w-11/12 relative border-1 shadow-md p-4 rounded-2xl mb-8">
+      <div className="mx-auto w-11/12 border-1 shadow-md p-4 rounded-2xl mb-8 flex flex-col">
         <h1 className="text-3xl font-bold text-center mb-3 -mt-1 text-primary">Lista de cursos</h1>
+        <div className="flex flex-row flex-wrap gap-4 justify-center">
+          {/*Tarjeta*/}
+          {cursosDocente.map((curso) => (
+            <div className="rounded-xl bg-slate flex flex-col border-2 px-5 py-8 max-w-[260px] min-w-[260px] justify-between">
+              <div>
+                <h1 className="text-xl font-bold whitespace-pre-wrap text-center">{curso?.nombre ?? ""}</h1>
+              </div>
+              <div>
+                <h2 className="text-center text-neutral-500">{curso?.descripcion ?? ""}</h2>
+                <div className="h-0 w-full border-1 border-neutral-200 my-2"></div>
+                <div className="flex gap-y-1 flex-col mb-3">
+                  <h3 className="text-sm flex gap-x-1">
+                    <svg className="mt-1" viewBox="0 0 1024 1024" fill="currentColor" height="1em" width="1em">
+                      <path d="M112 880c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V460H112v420zm768-696H712v-64c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v64H384v-64c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v64H144c-17.7 0-32 14.3-32 32v176h800V216c0-17.7-14.3-32-32-32z" />
+                    </svg>
+                    {curso?.fechaInicio.replace(/(\d{4})-(\d{2})-(\d{2})/, "$3/$2/$1") ?? ""} -{" "}
+                    {curso?.fechaFinal.replace(/(\d{4})-(\d{2})-(\d{2})/, "$3/$2/$1") ?? ""}
+                  </h3>
+                  <h3 className="text-sm flex gap-x-1">
+                    <svg viewBox="0 0 1024 1024" fill="currentColor" height="1em" width="1em" className="mt-1">
+                      <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm176.5 585.7l-28.6 39a7.99 7.99 0 01-11.2 1.7L483.3 569.8a7.92 7.92 0 01-3.3-6.5V288c0-4.4 3.6-8 8-8h48.1c4.4 0 8 3.6 8 8v247.5l142.6 103.1c3.6 2.5 4.4 7.5 1.8 11.1z" />
+                    </svg>
+                    {curso?.horario[0] ?? ""} - {curso?.horario[curso.horario.length - 1] ?? ""}
+                  </h3>
+                  <h3 className="text-sm flex gap-x-1">
+                    <svg className="mt-1" viewBox="0 0 640 512" fill="currentColor" height="1em" width="1em">
+                      <path d="M0 488V171.3c0-26.2 15.9-49.7 40.2-59.4L308.1 4.8c7.6-3.1 16.1-3.1 23.8 0l267.9 107.1c24.3 9.7 40.2 33.3 40.2 59.4V488c0 13.3-10.7 24-24 24h-48c-13.3 0-24-10.7-24-24V224c0-17.7-14.3-32-32-32H128c-17.7 0-32 14.3-32 32v264c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24zm488 24H152c-13.3 0-24-10.7-24-24v-56h384v56c0 13.3-10.7 24-24 24zM128 400v-64h384v64H128zm0-96v-80h384v80H128z" />
+                    </svg>
+                    {curso?.taller ?? ""}
+                  </h3>
+                </div>
+              </div>
+              <div className="flex flex-row flex-nowrap">
+                {diasSemana.map((dia, index) => {
+                  const estaEnArray = curso.dias.includes(dia);
+                  const clase = estaEnArray ? diasColores[dia] : "bg-neutral-100 text-neutral-300";
+
+                  return (
+                    <div
+                      key={index}
+                      className={`h-full w-full flex align-middle border-1 border-neutral-400 justify-center place-content-center ${clase}`}
+                    >
+                      <p>{dia.charAt(0)}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
 };
 
-export default DocenteCursos;
+export default DocentecursosDocente;
